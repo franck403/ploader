00ed939c9f1d77155589d0b3b55a978b926d53fd
franck403 <79372051+franck403@users.noreply.github.com>
2023-02-24 17:23:54 -0500

    Update and rename main.js to loader.main.js

---
+++ loader.main.js
@@ -0,0 +1,7 @@
+function load(url,id_to_place_content){
+  const id = id_to_place_content
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => document.getElementById(id).innerHTML = data);
+}
+console.log("[info] ploader is started. command are load(url,id where content is place) ")
--- main.js
+++
@@ -1,9 +0,0 @@
-function url() {
-  function load(url,id_to_place_content){
-    const id = id_to_place_content
-    fetch(url)
-      .then((response) => response.text())
-      .then((data) => document.getElementById(id).innerHTML = data);
-  }
-}
-console.log("[info] ploader is started. command are url.load(url,id where content is place) ")
